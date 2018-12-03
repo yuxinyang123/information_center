133f0481210da076728ea024ca1f48a34f56cdf5
wangzixuan <798647567@qq.com>
2018-12-03 14:13:30 +0800

    单线程定时任务demo

---
+++ src/test/java/com/hnu/softwarecollege/infocenter/schedutask/schedudemo.java
@@ -0,0 +1,29 @@
+package com.hnu.softwarecollege.infocenter.schedutask;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName schedudemo
+ * @Description TODO
+ * @Author wang
+ * @Date 2018/12/3 9:51
+ * @Version 1.0
+ **/
+
+
+public class schedudemo {
+    public static void main(String[] args){
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("hello!");
+            }
+        };
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+
+        service.scheduleAtFixedRate(runnable,0,10, TimeUnit.SECONDS);
+        //service.schedule(runnable,long delay,TimeUnit.MILLISECONDS);
+    }
+}
