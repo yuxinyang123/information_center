a98818a8ad71657e368e6687dfd20af8ba1a613d
wangzixuan <798647567@qq.com>
2018-12-12 10:11:36 +0800

    首页4个标签

---
+++ src/main/java/com/hnu/softwarecollege/infocenter/entity/vo/FourTag.java
@@ -0,0 +1,47 @@
+package com.hnu.softwarecollege.infocenter.entity.vo;
+
+/**
+ * @ClassName FourTag
+ * @Description TODO
+ * @Author wang
+ * @Date 2018/12/12 9:23
+ * @Version 1.0
+ **/
+public class FourTag {
+    private Double avggrade;
+    private Double havacredit;
+    private Double performancepoint;
+    private Double choosecredit;
+
+    public Double getAvggrade() {
+        return avggrade;
+    }
+
+    public void setAvggrade(Double avggrade) {
+        this.avggrade = avggrade;
+    }
+
+    public Double getHavacredit() {
+        return havacredit;
+    }
+
+    public void setHavacredit(Double havacredit) {
+        this.havacredit = havacredit;
+    }
+
+    public Double getPerformancepoint() {
+        return performancepoint;
+    }
+
+    public void setPerformancepoint(Double performancepoint) {
+        this.performancepoint = performancepoint;
+    }
+
+    public Double getChoosecredit() {
+        return choosecredit;
+    }
+
+    public void setChoosecredit(Double choosecredit) {
+        this.choosecredit = choosecredit;
+    }
+}
