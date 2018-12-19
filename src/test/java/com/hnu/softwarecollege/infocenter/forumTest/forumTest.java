46c1d9509790dc29094c60f159432020f44774ca
liuyashuang <741042741@qq.com>
2018-12-19 08:57:23 +0800

    配置文件auth 改为true

--- src/test/java/com/hnu/softwarecollege/infocenter/forumTest/forumTest.java
+++ src/test/java/com/hnu/softwarecollege/infocenter/forumTest/forumTest.java
@@ -3,8 +3,10 @@ package com.hnu.softwarecollege.infocenter.forumTest;
 import com.hnu.softwarecollege.infocenter.context.ThreadContext;
 import com.hnu.softwarecollege.infocenter.controller.ResourceController;
 import com.hnu.softwarecollege.infocenter.entity.po.UserPo;
+import com.hnu.softwarecollege.infocenter.entity.po.WeatherPo;
 import com.hnu.softwarecollege.infocenter.entity.vo.BaseResponseVo;
 import com.hnu.softwarecollege.infocenter.entity.vo.CommentForm;
+import com.hnu.softwarecollege.infocenter.mapper.WeatherPoMapper;
 import com.hnu.softwarecollege.infocenter.service.ResourceService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
@@ -31,6 +33,8 @@ public class forumTest {
     ResourceService resourceService;
     @Resource
     ResourceController resourceController;
+    @Resource
+    WeatherPoMapper weatherPoMapper;
     @Before
     public void createUser(){
         UserPo userPo = new UserPo(1l,"liuyashuang","liuyashuang@sina.com",
@@ -56,5 +60,17 @@ public class forumTest {
     public void getAllComment(){
         BaseResponseVo baseResponseVo = resourceController.getAllComments(1,3,"1");
         System.out.println(baseResponseVo);
+    }
+
+    @Test
+    public void updateWeather(){
+        WeatherPo weatherPo = new WeatherPo(101090101,"石家庄","19日星期三",
+                "07:13","高温 15.0℃","低温 2.0℃","8",
+                "17:04",240f,"阴晴之间，谨防紫外线侵扰",
+                "多云","<3级","西风");
+
+        int num = weatherPoMapper.updateByPrimaryKeySelective(weatherPo);
+        System.out.println(num);
+
     }
 }
