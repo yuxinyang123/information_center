bae627f2a07647517647e1aea36ccb3afaa87f57
yuxinyang123 <874733577@qq.com>
2018-12-23 00:12:11 +0800

    update addFace page

--- src/view/update/update-face.vue
+++ src/view/update/update-face.vue
@@ -1,77 +1,74 @@
 <template>
   <Row :gutter="10">
     <i-col span="12">
-      <Card>
-        <div class="update-paste-con">
-          <paste-editor v-model="pasteDataArr" @on-success="handleSuccess" @on-error="handleError"/>
-        </div>
-        <div class="update-paste-btn-con">
-          <span class="paste-tip">使用Tab键换列，使用回车键换行</span>
-          <Button type="primary" style="float: right;" @click="handleShow">显示表格数据</Button>
-        </div>
+      <Card shadow>
+        <video ref="video" id="video" width="480" height="320"></video>
       </Card>
     </i-col>
     <i-col span="12">
-      <Card>
-        <Table :height="400" :columns="columns" :data="tableData"/>
+      <Card shadow>
+        <canvas id="canvas" width="480" height="320"></canvas>
       </Card>
     </i-col>
   </Row>
 </template>
 
 <script>
-import PasteEditor from '_c/paste-editor'
-import { getTableDataFromArray } from '@/libs/util'
 export default {
-  name: 'update_paste_page',
-  components: {
-    PasteEditor
-  },
-  data () {
-    return {
-      pasteDataArr: [],
-      columns: [],
-      tableData: [],
-      validated: true,
-      errorIndex: 0
-    }
+  name: "update_paste_page",
+  components: {},
+  data() {
+    return {};
   },
   methods: {
-    handleSuccess () {
-      this.validated = true
-    },
-    handleError (index) {
-      this.validated = false
-      this.errorIndex = index
-    },
-    handleShow () {
-      if (!this.validated) {
-        this.$Notice.error({
-          title: '您的内容不规范',
-          desc: `您的第${this.errorIndex + 1}行数据不规范，请修改`
+    openVideo() {
+      let video = $("#video")[0];
+      navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            width: 480,
+            height: 320
+          }
         })
-      } else {
-        let { columns, tableData } = getTableDataFromArray(this.pasteDataArr)
-        this.columns = columns
-        this.tableData = tableData
-      }
-    }
+        .then(
+          stream => {
+            video.srcObject = stream;
+            video.play();
+          },
+          e => {
+            console.error(e);
+          }
+        );
+    },
+    authFace() {
+      let context = $("#canvas")[0].getContext("2d");
+      context.drawImage(video, 0, 0, 480, 320);
+      this.form.pic = $("#canvas")[0].toDataURL();
+      this.$emit("handleAuth", this.form);
+    },
+    stopVideo() {
+      let video = this.$refs.video;
+      console.log(video);
+      video.srcObject.getTracks()[0].stop();
+    },
+    ok() {},
+    cancle() {}
+  },
+  mounted() {
+    this.openVideo();
+    console.log($("#video")[0]);
+  },
+  beforeDestroy() {
+    console.log("before destoryed");
+    console.log("jq" + $("#video")[0]);
+    console.log("ref:" + this.$refs.video);
+    this.stopVideo();
+  },
+  destroyed() {
+    console.log("destoryed");
+    console.log("jq" + $("#video")[0]);
+    console.log("ref:" + this.$refs.video);
   }
-}
+};
 </script>
-
-<style lang="less">
-.update-paste{
-  &-con{
-    height: 350px;
-  }
-  &-btn-con{
-    box-sizing: content-box;
-    height: 30px;
-    padding: 15px 0 5px;
-  }
-}
-.paste-tip{
-  color: #19be6b;
-}
-</style>
