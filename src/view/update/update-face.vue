d3e7a2d64f37fc31b2e0a6b9e13d84da63a39c02
yuxinyang123 <874733577@qq.com>
2018-12-23 14:38:42 +0800

    update addFace page

--- src/view/update/update-face.vue
+++ src/view/update/update-face.vue
@@ -2,23 +2,46 @@
   <Row :gutter="10">
     <i-col span="12">
       <Card shadow>
-        <video ref="video" id="video" width="480" height="320"></video>
+        <h1>拍摄人脸</h1>
+        <Row type="flex" justify="center">
+          <Col>
+            <video ref="video" id="video" width="480" height="320"></video>
+          </Col>
+        </Row>
+        <Row type="flex" justify="center">
+          <Col>
+            <Button type="primary" @click="drawImage()">拍摄</Button>
+          </Col>
+        </Row>
       </Card>
     </i-col>
     <i-col span="12">
       <Card shadow>
-        <canvas id="canvas" width="480" height="320"></canvas>
+        <h1>上传人脸</h1>
+        <Row type="flex" justify="center">
+          <Col>
+            <canvas id="canvas" width="480" height="320"></canvas>
+          </Col>
+        </Row>
+        <Row type="flex" justify="center">
+          <Col>
+            <Button @click="send()">添加</Button>
+          </Col>
+        </Row>
       </Card>
     </i-col>
   </Row>
 </template>
 
 <script>
+import { addUserFace } from "@/api/user";
 export default {
   name: "update_paste_page",
   components: {},
   data() {
-    return {};
+    return {
+      pic: String
+    };
   },
   methods: {
     openVideo() {
@@ -41,34 +64,31 @@ export default {
           }
         );
     },
-    authFace() {
+    drawImage() {
       let context = $("#canvas")[0].getContext("2d");
       context.drawImage(video, 0, 0, 480, 320);
-      this.form.pic = $("#canvas")[0].toDataURL();
-      this.$emit("handleAuth", this.form);
+      this.pic = $("#canvas")[0].toDataURL();
     },
     stopVideo() {
       let video = this.$refs.video;
-      console.log(video);
       video.srcObject.getTracks()[0].stop();
     },
-    ok() {},
-    cancle() {}
+    send() {
+      addUserFace(this.pic).then(res => {
+        res = res.data.data;
+        if (res.result.face_token != null) {
+          this.$Message.success("注册成功");
+        } else {
+          this.$Message.error(res.error_msg);
+        }
+      });
+    }
   },
   mounted() {
     this.openVideo();
-    console.log($("#video")[0]);
   },
   beforeDestroy() {
-    console.log("before destoryed");
-    console.log("jq" + $("#video")[0]);
-    console.log("ref:" + this.$refs.video);
     this.stopVideo();
-  },
-  destroyed() {
-    console.log("destoryed");
-    console.log("jq" + $("#video")[0]);
-    console.log("ref:" + this.$refs.video);
   }
 };
 </script>
