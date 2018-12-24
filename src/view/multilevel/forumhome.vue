aab6bfe64d4940bf1eb81315eea10f8ae328deec
yuanyunpeng <15226551376@163.com>
2018-12-24 10:13:13 +0800

    修改

--- src/view/multilevel/forumhome.vue
+++ src/view/multilevel/forumhome.vue
@@ -1,72 +1,86 @@
 <template>
-<div>
-  <i-table :columns="columns1" :data="data1"></i-table>
-</div>
+  <div>
+    <i-table :columns="columns1" :data="data1"></i-table>
+  </div>
 </template>
 <script>
-export default {
-  name: 'forumhome_page',
-  data () {
-            return {
-                columns1: [
-                    {
-                        title: '编号',
-                        key: 'num'
-                    },
-                    {
-                        title: '标题',
-                        key: 'tittle'
-                    },
-                    {
-                    	title:'发布者',
-                    	key:'author'
-                    },
-                    {
-                        title: '时间',
-                        key: 'data'
-                    }
-                ],
-                data1: [
-                    {
-                        num: '1',
-                        tittle: '行走在川西',
-                        author:'摄太郎',
-                        data: '2018.11.27'
-                    },
-                    {
-                        num: '1',
-                        tittle: '行走在川西',
-                        author:'摄太郎',
-                        data: '2018.11.27'
-                    },{
-                        num: '1',
-                        tittle: '行走在川西',
-                        author:'摄太郎',
-                        data: '2018.11.27'
-                    },{
-                        num: '1',
-                        tittle: '行走在川西',
-                        author:'摄太郎',
-                        data: '2018.11.27'
-                    },{
-                        num: '1',
-                        tittle: '行走在川西',
-                        author:'摄太郎',
-                        data: '2018.11.27'
-                    },{
-                        num: '1',
-                        tittle: '行走在川西',
-                        author:'摄太郎',
-                        data: '2018.11.27'
-                    },{
-                        num: '1',
-                        tittle: '行走在川西',
-                        author:'摄太郎',
-                        data: '2018.11.27'
-                    },
-                ]
-            }
-        }
+  import {
+    getlists
+  } from "@/api/data";
+  export default {
+    name: 'forumhome_page',
+    data() {
+      return {
+        columns1: [{
+            title: '编号',
+            key: 'num'
+          },
+          {
+            title: '标题',
+            key: 'tittle'
+          },
+          {
+            title: '发布者',
+            key: 'author'
+          },
+          {
+            title: '时间',
+            key: 'data'
+          }
+        ],
+        data1: [{
+            num: '1',
+            tittle: '行走在川西',
+            author: '摄太郎',
+            data: '2018.11.27'
+          },
+          {
+            num: '1',
+            tittle: '行走在川西',
+            author: '摄太郎',
+            data: '2018.11.27'
+          }, {
+            num: '1',
+            tittle: '行走在川西',
+            author: '摄太郎',
+            data: '2018.11.27'
+          }, {
+            num: '1',
+            tittle: '行走在川西',
+            author: '摄太郎',
+            data: '2018.11.27'
+          }, {
+            num: '1',
+            tittle: '行走在川西',
+            author: '摄太郎',
+            data: '2018.11.27'
+          }, {
+            num: '1',
+            tittle: '行走在川西',
+            author: '摄太郎',
+            data: '2018.11.27'
+          }, {
+            num: '1',
+            tittle: '行走在川西',
+            author: '摄太郎',
+            data: '2018.11.27'
+          },
+        ]
+      }
+    },
+    methods: {
+      handlegetLists(type) {
+        return new Promise((resolve, reject) => {
+          getlists(type).then(res => {
+            res = res.data.data;
+            resolve();
+          }).catch(err => {
+            console.log(err);
+            reject(err);
+          });
+        });
+      },
     }
+  }
 
 </script>
