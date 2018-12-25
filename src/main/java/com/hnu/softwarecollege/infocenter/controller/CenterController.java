668286e5dbc6719d8af25f54286a019db1db03cb
wangzixuan <798647567@qq.com>
2018-12-25 11:21:09 +0800

    修改hotpot

--- src/main/java/com/hnu/softwarecollege/infocenter/controller/CenterController.java
+++ src/main/java/com/hnu/softwarecollege/infocenter/controller/CenterController.java
@@ -11,11 +11,9 @@ import com.hnu.softwarecollege.infocenter.mapper.CenterPoMapper;
 import com.hnu.softwarecollege.infocenter.service.CenterService;
 import com.hnu.softwarecollege.infocenter.service.WeatherService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 /**
@@ -159,21 +157,15 @@ public class CenterController {
      * @return com.hnu.softwarecollege.infocenter.entity.vo.BaseResponseVo
      **/
     @GetMapping("/hotpot")
-    public BaseResponseVo getHotpot(@NotEmpty @RequestParam int pageNum, @NotEmpty @RequestParam int pageSize, Errors errors){
-        if (errors.hasErrors()){
-            return BaseResponseVo.error("page code is null");
-        }
+    public BaseResponseVo getHotpot(@RequestParam int pageNum, @RequestParam int pageSize){
         List<HotsPotPo> hotsPotPoList;
 
         hotsPotPoList = centerService.getHotPot(pageNum,pageSize);
-
-//        if(hotsPotPoList != null){
-//            log.info("ok");
-//        }
-//        for(HotsPotPo po : hotsPotPoList){
-//            System.out.print(po.getHotspotTitle());
-//        }
-        return BaseResponseVo.success(hotsPotPoList);
+        if (hotsPotPoList.size() == 0){
+            return BaseResponseVo.error("hotpot not null");
+        }else {
+            return BaseResponseVo.success(hotsPotPoList);
+        }
     }
 
     /*
