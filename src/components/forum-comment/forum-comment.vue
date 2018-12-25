24eace5ff57cc1b25b71dae1029462ce89d3bf7a
yuanyunpeng <15226551376@163.com>
2018-12-25 17:18:16 +0800

    修改父子组件

--- src/components/forum-comment/forum-comment.vue
+++ src/components/forum-comment/forum-comment.vue
@@ -1,42 +1,24 @@
 <template>
   <div>
-    
+
   </div>
 </template>
 <script>
-  import {
-    getComment
-  } from "@/api/data";
   export default {
     props: {
 
     },
     data() {
       return {
-        pageNum: 0,
-        pageSize: 0,
-        essayId: 0,
-        lists: [],
-       
+
+
       }
     },
     methods: {
-      handleGetComment() {
-        getComment(this.pageNum, this.pageSize, this.essayId)
-          .then(res => {
-            res = res.data.data;
-            this.lists = res.list
-            console.log(res)
 
-          })
-          .catch(err => {
-            console.log(err);
-            reject(err);
-          });
-      }
     },
     mounted() {
-      this.handleGetComment()
+
     }
   }
 
--- src/view/forum/content.vue
+++ src/view/forum/content.vue
@@ -1,33 +1,55 @@
 <template>
-<div>
-  <forum-content></forum-content>
-  <forum-comment></forum-comment>
+  <div>
+    <forum-content></forum-content>
+    <forum-comment></forum-comment>
   </div>
 </template>
 <script>
-import ForumContent from "_c/forum-content";
-import ForumComment from "_c/forum-comment";
-export default {
-  components: {
-    ForumContent,
-    ForumComment
-  },
-  props: {
-    id: String,
-    type: String
-  },
-  data() {
-    return {
-      title: "this is title"
-    };
-  },
-  methods: {},
-  created() {
-    // console.log(this.$router.currentRoute.meta.title);
-  },
-  mounted() {
-    // console.log(this.id, this.type);
-  }
-};
-</script>
+  import ForumContent from "_c/forum-content";
+  import ForumComment from "_c/forum-comment";
+  import {
+    getComment
+  } from "@/api/data";
+  export default {
+    components: {
+      ForumContent,
+      ForumComment
+    },
+    props: {
+      id: String,
+      type: String
+    },
+    data() {
+      return {
+        title: "this is title",
+        pageNum: 0,
+        pageSize: 0,
+        essayId: 0,
+        lists: [],
+      };
+    },
+    methods: {
+       handleGetComment() {
+        getComment(this.pageNum, this.pageSize, this.essayId)
+          .then(res => {
+            res = res.data.data;
+            this.lists = res.list
+            console.log(res)
 
+          })
+          .catch(err => {
+            console.log(err);
+            reject(err);
+          });
+      }
+    },
+    created() {
+      // console.log(this.$router.currentRoute.meta.title);
+    },
+    mounted() {
+       this.handleGetComment()
+      // console.log(this.id, this.type);
+    }
+  };
+
+</script>
